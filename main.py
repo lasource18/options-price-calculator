1f985e9ed4d764a99991d8daedad5f1af251b497
lasource18 <cmguinan@yahoo.fr>
2023-08-27 21:25:00 -0400

    "Modified option class, added charts etc"

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 # coding: utf-8
 # main.py
 
-from options.options import Options
+from options.options import Option
 from flask import Flask
 from flask import request, render_template, json
 from werkzeug.exceptions import HTTPException
+import numpy as np
+import pandas as pd
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -14,24 +17,27 @@ def index():
     if request.method == 'POST':
         contract_type = request.form['contract_type']
         market_price = float(request.form['market_price'])
-        stock_price = float(request.form['stock_price'])
+        spot = float(request.form['stock_price'])
         strike = float(request.form['strike'])
-        exp = int(request.form['exp'])
-        rf_rate = float(request.form['rf_rate'])
+        dte = (datetime.strptime(request.form['exp'], '%Y-%m-%d') - datetime.today()).days/365
+        rate = float(request.form['rf_rate'])
         vol = float(request.form['vol'])
         div = float(request.form['div'])
 
-        price, price_diff = pricing(contract_type, market_price, stock_price, strike, exp, rf_rate, vol, div)
+        option = Option(spot, strike, rate, dte, vol, div)
 
-        opt_price = str(round(price['value']['Option Price'], 2))
-        intrinsic_value = str(round(price['value']['Intrinsic Value'], 2))
-        time_value = str(round(price['value']['Time Value'], 2))
+        opt_price = round(option.callPrice, 2) if contract_type == 'C' else round(option.putPrice, 2)
+        intrinsic_value = round(max(spot - strike, 0), 2) if contract_type == 'C' else round(max(strike - spot, 0), 2)
+        time_value = round(abs(intrinsic_value - opt_price), 2)
 
-        delta = str(round(price['greeks']['Delta'], 4))
-        gamma = str(round(price['greeks']['Gamma'], 4))
-        vega = str(round(price['greeks']['Vega'], 4))
-        theta = str(round(price['greeks']['Theta'], 4))
-        rho = str(round(price['greeks']['Rho'], 4))
+        delta = str(round(option.callDelta, 4)) if contract_type == 'C' else str(round(option.putDelta, 4))
+        gamma = str(round(option.gamma, 4))
+        vega = str(round(option.vega, 4))
+        theta = str(round(option.callTheta, 4)) if contract_type == 'C' else str(round(option.putTheta, 4))
+        rho = str(round(option.callRho, 4)) if contract_type == 'C' else str(round(option.putRho, 4))
+        div_sens = str(round(option.callDivSens, 4)) if contract_type == 'C' else str(round(option.putDivSens, 4))
+
+        price_diff = (opt_price - market_price) / market_price
 
         if price_diff > 0:
             difference = f'The option is undervalued by {abs(price_diff):.2%}.'
@@ -42,8 +48,16 @@ def index():
         else:
             difference = f'The option is overvalued by {abs(price_diff):.2%}.'
 
+        opt_price = str(opt_price)
+        intrinsic_value = str(intrinsic_value)
+
+        data = graph(contract_type, int(spot * 1.2), int(spot * 0.8), strike, rate, vol, dte, div)
+
+        print(data)
+
         return render_template('index.html',
                                opt_price=opt_price,
+                               market_price=market_price,
                                intrinsic_value=intrinsic_value,
                                time_value=time_value,
                                delta=delta,
@@ -51,7 +65,9 @@ def index():
                                vega=vega,
                                theta=theta,
                                rho=rho,
-                               difference=difference
+                               div_sens=div_sens,
+                               difference=difference,
+                               data=data
                 )
                                 
     elif request.method == 'GET':
@@ -71,17 +87,28 @@ def handle_exception(e):
     response.content_type = "application/json"
     return response   
 
-def pricing(contract_type, market_price, stock_price, strike, exp, rf_rate, vol, div):
-    # Create the option object
-    option = Options(contract_type, market_price, stock_price, strike, exp, rf_rate, vol, div)
+def graph(contract_type, upper_bound, lower_bound, strike, rate, vol, dte, div):
+    df = pd.DataFrame({'Strike':np.arange(lower_bound, upper_bound)})
+    df['Delta'] = df['Gamma'] = df['Vega'] = df['Theta'] = df['Rho'] = df['Dividend Sensitivity'] = 0
 
-    # Calculate option's price
-    price = option.option_price()
+    for i in range(len(df)):
+        option = Option(df.loc[i, 'Strike'], strike, rate, dte, vol, div)
+        if contract_type == 'C':
+            df.loc[i, 'Delta'] = option.callDelta
+            df.loc[i, 'Theta'] = option.callTheta
+            df.loc[i, 'Rho'] = option.callRho
+            df.loc[i, 'Dividend Sensitivity'] = option.callDivSens
+        else:
+            df.loc[i, 'Delta'] = option.putDelta
+            df.loc[i, 'Theta'] = option.putTheta
+            df.loc[i, 'Rho'] = option.putRho
+            df.loc[i, 'Dividend Sensitivity'] = option.putDivSens
+        df.loc[i, 'Gamma'] = option.gamma
+        df.loc[i, 'Vega'] = option.vega
+    
+    d = {key:list(df[key]) for key in df.columns}
 
-    # Calculate the price difference between observed price and theoritical price
-    price_diff = (price['value']['Option Price'] - option.market_price) / option.market_price
-
-    return price, price_diff
+    return d
     
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=5000, debug=True)
--- options/options.py
+++ options/options.py
@@ -5,100 +5,124 @@
 #
 
 import math
-from scipy.special import ndtr
+from scipy.stats import norm
 
-class Options:
+class Option:
     """
-    This class models a vanilla European option contract.
-    It provides a method called option_price() to calculate the theoritical price of the contract.
-    The calculation of the contract's price relies on the Blach-Scholes framework
-    to value an European option contract on a dividend/non-dividend paying stock.
-
-    Useful links
-    ------------
-    Options: https://www.investopedia.com/options-basics-tutorial-4583012
-    Black-Scholes model: https://www.investopedia.com/terms/b/blackscholes.asp
-    Greeks: https://www.optionsplaybook.com/options-introduction/option-greeks/
+        This is a class for Options contract for pricing European options on stocks without dividends.
+        Attributes:
+        spot : int or float
+        strike : int or float
+        rate : float
+        dte : int or float [days to expiration in number of years]
+        vol : float
+        div : float
     """
-    def __init__(self, contract_type : str, market_price : float, stock_price : float, strike : float, exp : int, rf_rate : float, vol : float, div : float):
-        """
-        Parameters
-        ----------
-        contract_type (str): call or put
-        stock_price (float): Current Stock Price 
-        strike (float): Exercise price 
-        exp (int): Time to expiration in days 
-        rf_rate (float): Risk-free rate 
-        vol (float): Volatility 
-        div (float): Dividend Yield
-        """
-        self.contract_type = contract_type
-        self.market_price = market_price
-        self.stock_price = stock_price
+    def __init__(self, spot, strike, rate, dte, vol, div):
+        # Spot Price
+        self.spot = spot
+        # Option Strike
         self.strike = strike
-        self.exp = exp / 252
-        self.rf_rate = rf_rate / 100
+        #Interest rate
+        self.rate = rate / 100
+        # Days To Expiry
+        self.dte = dte
+        # Volatility
         self.vol = vol / 100
+        # Dividend
         self.div = div / 100
-
-    def option_price(self):
-        """
-        This method as the name suggests is used calculate the price of an option.
-        The formula used will depend on the type of contract. That is:
-        - Call: Se^(-qt)*N(d1) - Ke^(-rt)*N(d2)
-        - Put: Ke^(-rt)*N(-d2) - Se^(-qt)*N(-d1)
-        This method also calculate the options' greeks (delta, gamma, theta, rho).
+        # Utility
+        self._a_ = self.vol * self.dte ** 0.5
         
-        Parameters
-        ----------
-        None
-
-        Returns the option's theoritical price, intrinsic value and time value
-        and the greeks of the option as a dictionary.
-        """
-        d1 = (math.log(self.stock_price/self.strike) + (self.rf_rate - self.div + (pow(self.vol,2))/2))/(self.vol * math.sqrt(self.exp))
-        d2 = d1 - self.vol * math.sqrt(self.exp)
-
-        A = self.stock_price * math.exp(-self.div * self.exp)
-        B = self.strike * math.exp(-self.rf_rate * self.exp)
+        if self.strike == 0:
+            raise ZeroDivisionError('The strike price cannot be zero')
+        else:
+            self._d1_ = (math.log((self.spot/self.strike) + (self.rate - self.div + (self.vol)**2) / 2) * self.dte) / self._a_
         
-        # If contract_type = 'call' calculate the price of a call
-        if self.contract_type.lower() == 'call':
-            N_d1 = ndtr(d1)
-            N_d2 = ndtr(d2)
-            option_price = A * N_d1 - B * N_d2
-            intrinsic_val = max(self.stock_price - self.strike, 0)
-
-        # If contract_type = 'put' calculate the price of a put
-        elif self.contract_type.lower() == 'put':
-             N_d1 = ndtr(-d1)
-             N_d2 = ndtr(-d2)
-             option_price = B * N_d2 - A * N_d1
-             intrinsic_val = max(self.strike - self.stock_price, 0)
-
-        time_val = option_price - intrinsic_val
+        self._d2_ = self._d1_ - self._a_
+        self._b_ = math.e ** (-self.rate * self.dte)
+        self._c_ = math.e ** (-self.div * self.dte)
         
-        value = {'Option Price': option_price, 'Intrinsic Value': intrinsic_val, 'Time Value': time_val}
+        # The __dict__ attribute
+        '''
+            Contains all the attributes defined for the object itself. It maps the attribute name to its value
+        '''
+        for i in ['callPrice', 'putPrice', 'callDelta', 'putDelta', 'callTheta', 'putTheta', 'callRho', 'putRho', 'vega', 'gamma']:
+            self.__dict__[i] = None
+            
+            [self.callPrice, self.putPrice] = self._price
+            [self.callDelta, self.putDelta] = self._delta
+            [self.callTheta, self.putTheta] = self._theta
+            [self.callRho, self.putRho] = self._rho
+            [self.callDivSens, self.putDivSens] = self._div_sens
+            self.vega = self._vega
+            self.gamma = self._gamma
+            
+    # Option Price
+    @property
+    def _price(self):
+        ''' Returns the option price: [Call price, Put price]'''
+        if self.vol == 0 or self.dte == 0:
+            call = max(0.0, self.spot - self.strike)
+            call = max(0.0, self.strike - self.spot)
+        else:
+            call = self.spot * self._c_ * norm.cdf(self._d1_) - self.strike * self._b_ * norm.cdf(self._d2_)
+            put = self.strike * self._b_ * norm.cdf(-self._d2_) - self.spot * self._c_ * norm.cdf(-self._d1_)
 
-        # Calculate the greeks
-        if self.contract_type == 'call':
-            delta = N_d1 * math.exp(-self.div * self.exp)
-            theta = (-(((self.stock_price * self.vol * math.exp(-self.div*self.exp))/(2*math.sqrt(self.exp))) * ((math.exp(-pow(d1, 2)/2))/(2*math.pi) ))
-                     - (self.rf_rate*self.strike*math.exp(-self.exp*self.rf_rate) * N_d2 + self.stock_price * math.exp(-self.div*self.exp) * N_d1)) / 252
-            rho = (self.strike * self.exp * math.exp(-self.rf_rate*self.exp) * N_d2) / 100
+        return [call, put]
+
+    # Option Delta
+    @property
+    def _delta(self):
+        ''' Returns the option delta: [Call delta, Put delta]'''
+        if self.vol == 0 or self.dte == 0:
+            call = 1.0  if self.spot > self.strike else 0.0
+            put = -1.0 if self.spot < self.strike else 0.0
 
         else:
-            delta = (N_d1 - 1) * math.exp(-self.div * self.exp)
-            theta = (-(((self.stock_price * self.vol)/(2*math.sqrt(self.exp))) * ((math.exp(-pow(d1, 2)/2))/(2*math.pi) ))
-                     - (self.rf_rate*self.strike*math.exp(-self.exp*self.rf_rate) * N_d2 + self.stock_price * math.exp(-self.div*self.exp) * N_d1)) / 252
-            rho = -(self.strike * self.exp * math.exp(-self.rf_rate*self.exp) * N_d2) / 100
+            call = self._c_ * norm.cdf(self._d1_)
+            put = - self._c_ * norm.cdf(-self._d1_)
 
-        gamma = (math.exp(-self.div*self.exp) / self.stock_price*self.vol) * (1 / math.sqrt(2*math.pi) * math.exp(-pow(d1,2)/2))
-        vega = (1 / 100 * (self.stock_price*math.exp(-self.div*self.exp)*math.sqrt(self.exp))) * (1 / math.sqrt(2*math.pi) * math.exp(-pow(d1,2)/2))
+        return [call, put]
 
-        greeks = {'Delta': delta, 'Gamma': gamma, 'Theta': theta, 'Vega': vega, 'Rho': rho}
+    # Option Gamma
+    @property
+    def _gamma(self):
+        ''' Returns the option gamma'''
+        return self._c_ * norm.pdf(self._d1_) / (self.spot * self._a_)
 
-        result = {'value': value, 'greeks': greeks}
+    # Option Vega
+    @property
+    def _vega(self):
+        ''' Returns the option vega'''
+        if self.vol == 0 or self.dte == 0:
+            return 0.0
+        else:
+            return self.spot * self._c_ * norm.pdf(self._d1_) * self.dte**0.5 / 100
 
-        return result
+    # Option Theta
+    @property
+    def _theta(self):
+        ''' Returns the option theta: [Call theta, Put theta]'''
+        call =  (-1 * (self._c_ * self.spot * norm.pdf(self._d1_) * self.vol) / (2*self.dte**0.5)) + (self.div * self.spot * self._c_) - (self.rate * self.strike * self._b_ * norm.cdf(self._d2_))
+        put = (-1 * (self._c_ * -self.spot * norm.pdf(self._d1_) * self.vol) / (2*self.dte**0.5)) - (self.div * self.spot * self._c_) + (self.rate * self.strike * self._b_ * norm.cdf(self._d2_))
+
+        return [call / 365, put / 365]
+
+    # Option Rho
+    @property
+    def _rho(self):
+        ''' Returns the option rho: [Call rho, Put rho]'''
+        call = self.strike * self.dte * self._b_ * norm.cdf(self._d2_) / 100
+        put = -self.strike * self.dte * self._b_ * norm.cdf(-self._d2_) / 100
+
+        return [call, put]
+
+    @property
+    def _div_sens(self):
+        ''' Returns the option sensitivity to the dividend yield: [Call div_sens, Put div_sens]'''
+        call = -self.dte * self.spot * self._c_ * norm.cdf(self._d1_)
+        put = self.dte * self.spot * self._c_ * norm.cdf(-self._d1_)
+
+        return [call, put]
 
